3d2bd3a987de89cc08adfa9320d5d0f307020edc
Екатерина Филиппова <catherina1punk@gmail.com>
2022-01-24 16:18:54 +0300

    + unit testing

---
+++ src/core/routes/Router.test.js
@@ -0,0 +1,37 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const {Router} = require('./Router')
+const {Page} = require('../Page')
+
+class DashboardPage extends Page {
+  getRoot() {
+    const root = document.createElement('div')
+    root.innerHTML = 'dashboard'
+    return root
+  }
+}
+class ExcelPage extends Page {}
+
+describe('Router', () => {
+  let router
+  let $root
+
+  beforeEach(() => {
+    $root = document.createElement('div')
+    router = new Router($root, {
+      dashboard: DashboardPage,
+      excel: ExcelPage
+    })
+  })
+
+  test('should be defined', () => {
+    expect(router).toBeDefined()
+  })
+
+  test('should render Dashboard page', () => {
+    router.changePageHandler()
+    expect($root.innerHTML).toBe('<div>dashboard</div>')
+  })
+})
--- src/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  modulePaths: ['src'],
-  moduleNameMapper: {
-    '^src/(.*)': '<src>\\yourPath\\$1',
-  },
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\](?!@myBem)(.+)[/\\\\].+\\.(js|jsx)',
-  ],
-};
